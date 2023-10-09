11d00168a4fbc5e63ee1c5a02444ec7dbb457da6
timborovkov <tim.borovkov@icloud.com>
2023-10-09 10:46:43 +0200

    Create example protected route

---
+++ src/api/protected-route.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import {
+  ClerkExpressWithAuth,
+  LooseAuthProp,
+  WithAuthProp,
+} from "@clerk/clerk-sdk-node";
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+declare global {
+  namespace Express {
+    interface Request extends LooseAuthProp {}
+  }
+}
+
+router.get<{}, any>(
+  "/",
+  ClerkExpressWithAuth({
+    // audience: "https://plantrip.ai",
+    // strict: true,
+  }),
+  (req: WithAuthProp<Request>, res: Response) => {
+    res.json(req.auth);
+  }
+);
+
+export default router;
