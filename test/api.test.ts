fdd6426041f2fe59d354ad6b9b72a447c081ba7f
timborovkov <tim.borovkov@icloud.com>
2023-10-11 21:29:42 +0200

    Add test for /plan

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,25 +1,40 @@
-import request from 'supertest';
+import request from "supertest";
 
-import app from '../src/app';
+import app from "../src/app";
 
-describe('GET /api/v1', () => {
-  it('responds with a json message', (done) => {
+describe("GET /api/v1", () => {
+  it("responds with a json message", (done) => {
     request(app)
-      .get('/api/v1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: 'API - 👋🌎🌍🌏',
-      }, done);
+      .get("/api/v1")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(
+        200,
+        {
+          message: "API - 👋🌎🌍🌏",
+        },
+        done
+      );
   });
 });
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
+describe("GET /api/v1/emojis", () => {
+  it("responds with a json message", (done) => {
     request(app)
-      .get('/api/v1/emojis')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
+      .get("/api/v1/emojis")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, ["😀", "😳", "🙄"], done);
+  });
+});
+
+describe("POST /api/v1/plan", () => {
+  it("responds with a json message", (done) => {
+    request(app)
+      .post("/api/v1/plan")
+      .set("Accept", "application/json")
+      .set("Authorization", process.env.API_KEY ?? "")
+      .expect("Content-Type", /json/)
+      .expect(200, ["😀", "😳", "🙄"], done);
   });
 });
