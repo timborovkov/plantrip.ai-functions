966a9a49b63b471f49ca31f85e7b60e0e7f9be01
timborovkov <tim.borovkov@icloud.com>
2023-10-12 11:59:45 +0200

    Add basic cron logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import cron from "node-cron";
 import app from "./app";
 
+// Run every 5 minutes
+cron.schedule("* 5 * * *", () => {
+  console.log("Running cron job");
+});
+
+// Start the server
 const port = process.env.PORT || 3300;
 app.listen(port, () => {
   /* eslint-disable no-console */
