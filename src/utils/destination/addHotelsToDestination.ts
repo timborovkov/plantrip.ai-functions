cb1ee7468b5b367d51c3685dfbca30ec0a549323
timborovkov <tim.borovkov@icloud.com>
2024-01-05 12:23:42 +0200

    Create addHotelsToDestination.ts

---
+++ src/utils/destination/addHotelsToDestination.ts
@@ -0,0 +1,59 @@
+import prisma from "../prisma";
+import { Destination } from "@prisma/client";
+
+import AmadeusAPI from "../AmadeusAPI";
+const amadeus = new AmadeusAPI();
+
+export default async function addHotelsToDestination(
+  destinationPlace: google.maps.GeocoderResult,
+  theDestination: Destination
+) {
+  try {
+    // Get the destination with hotels
+    const destination = await prisma.destination.findFirst({
+      where: {
+        id: theDestination.id,
+      },
+      include: {
+        Hotels: true,
+      },
+    });
+    // Check if destination already has hotels
+    if (destination && destination?.Hotels?.length > 0) {
+      return destination.Hotels;
+    }
+    if (!destination) {
+      return [];
+    }
+    // No Hotels yet, fetch them
+    // Get "points of interest" and activity recommendations concurrently
+    const hotels = await amadeus.searchHotels({
+      radius: 10,
+      latitude: destinationPlace.geometry.location.lat as any as number,
+      longitude: destinationPlace.geometry.location.lng as any as number,
+    });
+    // Create POIs and hotels
+    const createHotelsList = [
+      ...hotels.map((hotel) => {
+        return {
+          name: hotel.name ?? "",
+          chainCode: hotel.chainCode ?? "",
+          iataCode: hotel.iataCode ?? "",
+          dupeId: hotel.dupeId ?? 0,
+          hotelId: hotel.hotelId ?? "",
+          geoCode: JSON.stringify(hotel.geoCode ?? ""),
+          address: JSON.stringify(hotel.address ?? ""),
+          metaData: "",
+          destinationId: destination?.id,
+        };
+      }),
+    ];
+    await prisma.hotels.createMany({
+      data: createHotelsList,
+    });
+    return createHotelsList;
+  } catch (error) {
+    console.error("addHotelsToDestination", error);
+    return [];
+  }
+}
