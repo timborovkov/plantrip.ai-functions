4d09f942681390e5ee4f4b27c022073a276a11c3
timborovkov <tim.borovkov@icloud.com>
2024-01-05 19:52:02 +0200

    Add try catch wrapper for llm requests

--- src/utils/llmRequests/getPlanSummaryUsingOutline.ts
+++ src/utils/llmRequests/getPlanSummaryUsingOutline.ts
@@ -7,44 +7,49 @@ export default async function getPlanSummaryUsingOutline({
 }: {
   planOutline: string;
 }) {
-  const promptMessages: ChatCompletionMessageParam[] = [
-    {
-      role: "system",
-      content: `You are a travel agent. Write a trip plan summary of a few sentences. Make it sound like a travel agency writen promotional text.`,
-    },
-    {
-      role: "user",
-      content: planOutline,
-    },
-  ];
-  const apiResponse = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo", // Model maximum tokens: 4097
-    messages: promptMessages,
-    temperature: 0, // randomness
-    max_tokens: 3597,
-  });
+  try {
+    const promptMessages: ChatCompletionMessageParam[] = [
+      {
+        role: "system",
+        content: `You are a travel agent. Write a trip plan summary of a few sentences. Make it sound like a travel agency writen promotional text.`,
+      },
+      {
+        role: "user",
+        content: planOutline,
+      },
+    ];
+    const apiResponse = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo", // Model maximum tokens: 4097
+      messages: promptMessages,
+      temperature: 0, // randomness
+      max_tokens: 1000,
+    });
 
-  const tripOutline = apiResponse.choices[0].message?.content?.trim() || "";
+    const tripOutline = apiResponse.choices[0].message?.content?.trim() || "";
 
-  // Save the chat to the database for future training
-  promptMessages.push({
-    role: "assistant",
-    content: tripOutline,
-  });
-  await prisma.languageModelRequests.create({
-    data: {
-      systemRequest: promptMessages
-        .filter((a) => a.role === "system")
-        .map((a) => a.content)
-        .join("; "),
-      request: promptMessages
-        .filter((a) => a.role === "user")
-        .map((a) => a.content)
-        .join("; "),
-      reply: tripOutline,
-      fullChatObject: JSON.stringify(promptMessages),
-    },
-  });
+    // Save the chat to the database for future training
+    promptMessages.push({
+      role: "assistant",
+      content: tripOutline,
+    });
+    await prisma.languageModelRequests.create({
+      data: {
+        systemRequest: promptMessages
+          .filter((a) => a.role === "system")
+          .map((a) => a.content)
+          .join("; "),
+        request: promptMessages
+          .filter((a) => a.role === "user")
+          .map((a) => a.content)
+          .join("; "),
+        reply: tripOutline,
+        fullChatObject: JSON.stringify(promptMessages),
+      },
+    });
 
-  return tripOutline;
+    return tripOutline;
+  } catch (error) {
+    console.error("getPlanSummaryUsingOutline", error);
+    return "";
+  }
 }
