81960a022633d2cc5c0a33e14874477aaa419e45
timborovkov <tim.borovkov@icloud.com>
2024-01-05 21:24:31 +0200

    Save the plan to database after generating using /newPlan

--- src/api/newPlan.ts
+++ src/api/newPlan.ts
@@ -7,12 +7,13 @@ import addHotelsToDestination from "../utils/destination/addHotelsToDestination"
 import getPlanOutline from "../utils/llmRequests/getPlanOutline";
 import getPlanSummaryUsingOutline from "../utils/llmRequests/getPlanSummaryUsingOutline";
 import getDayByDayPlanUsingOutline from "../utils/llmRequests/getDayByDayPlanUsingOutline";
+import prisma from "../utils/prisma";
+import connectPlanToDestination from "../utils/destination/connectPlanToDestination";
 
 const router = express.Router();
 
 type TravelPlanResponseData = {
   response: string;
-  tripId?: number;
   planId?: number;
 };
 
@@ -106,76 +107,162 @@ router.post(
     const specialRequests = req.body.specialRequests ?? "";
     const tripDurationDays = durationTypesInDays(duration);
 
+    // Check if plan already exists in the database
+    const existingPlan = await prisma.plan.findFirst({
+      where: {
+        destination: destination,
+        duration: duration,
+        type: tripType,
+        tripBudget: tripBudget,
+        accommodationBooking: accommodationBooking,
+        travelersCount: travelersCount,
+        specialRequests: specialRequests,
+      },
+    });
+
+    if (existingPlan) {
+      // Send API response
+      res.status(200).json({
+        response: "ok",
+        planId: existingPlan.id,
+      });
+      // Handle destination
+      if (destinationPlace && !existingPlan.planDestinationId) {
+        const theDestination = await createOrUpdateDestination(
+          destination,
+          destinationPlace
+        );
+        await connectPlanToDestination(theDestination, existingPlan);
+      }
+      return true;
+    }
+    // No existing plan, generate a new one
     try {
+      // Create a plan instance
+      const newPlan = await prisma.plan.create({
+        data: {
+          destination: destination,
+          duration: duration,
+          type: tripType,
+          content: "",
+          summary: "",
+          tripBudget: tripBudget,
+          accommodationBooking: accommodationBooking,
+          travelersCount: travelersCount,
+          specialRequests: specialRequests,
+        },
+      });
+
+      // Return response from API
+      res.status(200).json({
+        response: "ok",
+        planId: newPlan.id,
+      });
+
+      // Connect destination to the plan
       const theDestination = await createOrUpdateDestination(
         destination,
         destinationPlace
       );
-      if (theDestination) {
-        // Add activities to destination
-        const theActivities = await addActivitiesToDestination(
-          destinationPlace,
-          theDestination
-        );
-        // Add hotels to destination
-        const theHotels = await addHotelsToDestination(
-          destinationPlace,
-          theDestination
-        );
-        if (theActivities && theHotels) {
-          // Build an LLM readable query
-          // Create an empty array to store the properties
-          const properties = [];
-
-          // Add properties to the array if they are not empty
-          if (tripType && tripType !== "") {
-            properties.push(`Trip Type: ${tripType}`);
-          }
-          if (destination && destination !== "") {
-            properties.push(`Destination: ${destination}`);
-          }
-          if (duration && duration !== "") {
-            properties.push(`Duration: ${duration}`);
-          }
-          if (tripBudget && tripBudget !== "") {
-            properties.push(`Trip Budget: ${tripBudget}`);
-          }
-          if (accommodationBooking && accommodationBooking !== "") {
-            properties.push(`Booked Acommodation: ${accommodationBooking}`);
-          }
-          if (travelersCount !== 1) {
-            properties.push(`Travelers Count: ${travelersCount}`);
-          }
-          if (specialRequests && specialRequests !== "") {
-            properties.push(`Special Requests: ${specialRequests}`);
-          }
-
-          const planOutline = await getPlanOutline({
-            properties,
-            destination,
-            theActivities,
-          });
-          const planSummary = await getPlanSummaryUsingOutline({
-            planOutline: planOutline,
-          });
-          const getDayByDayPlan = await getDayByDayPlanUsingOutline({
-            planOutline,
-            durationInDays: tripDurationDays,
-            properties,
-          });
-        }
+      if (!theDestination) {
+        throw new Error("Failed to create/get destination");
       }
+
+      // Add activities and hotels to destination
+      const theActivities = await addActivitiesToDestination(
+        destinationPlace,
+        theDestination
+      );
+      const theHotels = await addHotelsToDestination(
+        destinationPlace,
+        theDestination
+      );
+      if (!theActivities || !theHotels) {
+        throw new Error("Failed to create/get activities and hotels");
+      }
+
+      // Aggregate all properties in to a single array of strings
+      const properties = [];
+      if (tripType && tripType !== "") {
+        properties.push(`Trip Type: ${tripType}`);
+      }
+      if (destination && destination !== "") {
+        properties.push(`Destination: ${destination}`);
+      }
+      if (duration && duration !== "") {
+        properties.push(`Duration: ${duration}`);
+      }
+      if (tripBudget && tripBudget !== "") {
+        properties.push(`Trip Budget: ${tripBudget}`);
+      }
+      if (accommodationBooking && accommodationBooking !== "") {
+        properties.push(`Booked Acommodation: ${accommodationBooking}`);
+      }
+      if (travelersCount !== 1) {
+        properties.push(`Travelers Count: ${travelersCount}`);
+      }
+      if (specialRequests && specialRequests !== "") {
+        properties.push(`Special Requests: ${specialRequests}`);
+      }
+
+      // Generate the plan content using the LLM
+      const planOutline = await getPlanOutline({
+        properties,
+        destination,
+        theActivities,
+      });
+      const planSummary = await getPlanSummaryUsingOutline({
+        planOutline: planOutline,
+      });
+      const getDayByDayPlan = await getDayByDayPlanUsingOutline({
+        planOutline,
+        durationInDays: tripDurationDays,
+        properties,
+      });
+
+      // Create everything in the database
+      await prisma.plan.update({
+        where: {
+          id: newPlan.id,
+        },
+        data: {
+          generated: true,
+          content: planOutline,
+          summary: planSummary,
+          PlanDay: {
+            create: getDayByDayPlan.map((day, i) => {
+              const dayNumber = i + 1;
+              const sections = day.map((part) => ({
+                title: part.title,
+                planDaySectionDetails: {
+                  create: part.sections.map((a) => ({
+                    content: a,
+                  })),
+                },
+              }));
+              return {
+                day: dayNumber,
+                PlanDaySections: {
+                  create: sections,
+                },
+              };
+            }),
+          },
+        },
+      });
+
+      // Plan generation fully complete
+      console.log(
+        "Plan generation fully complete! 🎉 \n Plan ID: " + newPlan.id
+      );
+      return true;
     } catch (error) {
       console.error(error);
       res.status(500).json({
-        response: "Error",
+        response: "Something went wrong",
       });
       return;
     }
-
-    res.status(200).json({
-      response: "Done",
-    });
   }
 );
 
