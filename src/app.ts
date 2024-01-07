e50887b85f4a5128e515c9591b84e8cd3090767e
timborovkov <tim.borovkov@icloud.com>
2024-01-07 16:58:41 +0200

    Complete sentry implementation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
+
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
@@ -16,6 +19,27 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+Sentry.init({
+  dsn: "https://c7b7896ca049fb9d0793f2e7ac2bcd64@o4506530520694784.ingest.sentry.io/4506530535768064",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.get<{}, MessageResponse>("/", (req, res) => {
   res.json({
     message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
@@ -24,6 +48,7 @@ app.get<{}, MessageResponse>("/", (req, res) => {
 
 app.use("/api/v1", api);
 
+app.use(Sentry.Handlers.errorHandler());
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
