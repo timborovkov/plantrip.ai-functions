94ffb14d9bf03108d90e59d3ef2e3a3a40e0babd
timborovkov <tim.borovkov@icloud.com>
2024-01-07 16:52:33 +0200

    Adding Sentry

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 import cron from "node-cron";
 import app from "./app";
 import removeUngeneratedPlans from "./crons/removeUngeneratedPlans";
 import removeTripsWithoutPlans from "./crons/removeTripsWithoutPlans";
 import removeUngeneratedDestinations from "./crons/removeUngeneratedDestinations";
+
+Sentry.init({
+  dsn: "https://c7b7896ca049fb9d0793f2e7ac2bcd64@o4506530520694784.ingest.sentry.io/4506530535768064",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 // Run every 5 minutes
 cron.schedule("* 5 * * *", () => {
