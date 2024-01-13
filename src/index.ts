739abae446c0a3c5af49ac7032c73363541e7821
timborovkov <tim.borovkov@icloud.com>
2024-01-13 20:29:18 +0200

    Disable cron jobs

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import cron from "node-cron";
 import app from "./app";
-import removeUngeneratedPlans from "./crons/removeUngeneratedPlans";
-import removeTripsWithoutPlans from "./crons/removeTripsWithoutPlans";
-import removeUngeneratedDestinations from "./crons/removeUngeneratedDestinations";
+// import cron from "node-cron";
+// import removeUngeneratedPlans from "./crons/removeUngeneratedPlans";
+// import removeTripsWithoutPlans from "./crons/removeTripsWithoutPlans";
+// import removeUngeneratedDestinations from "./crons/removeUngeneratedDestinations";
 
-// Run every 15 minutes
-cron.schedule("* 15 * * *", () => {
-  console.log("Running cron job");
-  (async () => {
-    await removeUngeneratedPlans();
-    console.log("Removed ungenerated plans");
-    await removeTripsWithoutPlans();
-    console.log("Removed trips without plans");
-    await removeUngeneratedDestinations();
-    console.log("Removed ungenerated destinations");
-  })();
-});
+// // Run every 15 minutes
+// cron.schedule("* 15 * * *", () => {
+//   console.log("Running cron job");
+//   (async () => {
+//     await removeUngeneratedPlans();
+//     console.log("Removed ungenerated plans");
+//     await removeTripsWithoutPlans();
+//     console.log("Removed trips without plans");
+//     await removeUngeneratedDestinations();
+//     console.log("Removed ungenerated destinations");
+//   })();
+// });
 
 // Start the server
 const port = process.env.PORT || 3300;
