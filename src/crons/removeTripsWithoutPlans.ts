6c26e7fa93e3685ffe075e1e2175f5211620b942
timborovkov <tim.borovkov@icloud.com>
2024-01-14 02:08:18 +0200

    Update removeTripsWithoutPlans.ts

--- src/crons/removeTripsWithoutPlans.ts
+++ src/crons/removeTripsWithoutPlans.ts
@@ -1,13 +1,21 @@
 import prisma from "../utils/prisma";
+
 export default async function removeTripsWithoutPlans() {
   const oneDayAgo = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
-  // Remove trips without plans
-  await prisma.trip.deleteMany({
-    where: {
-      OR: [{ planId: null }, { userId: null }],
-      createdAt: {
-        lt: oneDayAgo,
+
+  try {
+    // Remove trips without plans
+    const deletedTrips = await prisma.trip.deleteMany({
+      where: {
+        OR: [{ planId: null }, { userId: null }],
+        createdAt: {
+          lt: oneDayAgo,
+        },
       },
-    },
-  });
+    });
+
+    console.log(`Deleted ${deletedTrips.count} trips.`);
+  } catch (error) {
+    console.error("Failed to delete trips");
+  }
 }
