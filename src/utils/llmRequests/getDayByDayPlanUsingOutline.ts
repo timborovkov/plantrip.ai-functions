1326fe9b2d241517244433ccb9da677b3ef180d8
timborovkov <tim.borovkov@icloud.com>
2024-01-16 09:55:59 +0200

    Fix plan day generation

--- src/utils/llmRequests/getDayByDayPlanUsingOutline.ts
+++ src/utils/llmRequests/getDayByDayPlanUsingOutline.ts
@@ -3,28 +3,58 @@ import openai from "../openai";
 import { ChatCompletionMessageParam } from "openai/resources";
 
 const assistantSystemInput = `
-You are a travel agent. You will be provided a general plan of a trip by the user. Please write a detailed schedule for a day of the trip specified by the user. Don't mention anything from the other days. Don't add any sort of notes or anything, just the requested structure.
-Don't add the day number as a seperate title. This structure involves text divided into sections, each identified by a title (e.g., "Morning:", "Afternoon:", "Evening:"). Titles introduce the purpose of each section, followed by content.  
-The response should be always in JSON format.
-Return the information in this format:
+You are a travel planning bot. 
+
+You will be provided a general plan of a trip by the user. Please write a detailed schedule for a day of the trip specified by the user. Don't mention anything from the other days. 
+
+Expand on the activities provided in the outline by the user, add more options. Improve the user provided plan. Each section content should have at least 4-5 items. Tell more about the plan items in the content section of the JSON.
+
+Don't add the day number as a seperate title. This structure involves text divided into sections, each identified by a title (e.g., "Morning:", "Afternoon:", "Evening:", "Dining options:", "Other"...). Titles introduce the purpose of each section, followed by content.  
+The response should be always in JSON format. Each day should have at least sections Morning, Afternoon, Evening and Notes
+Respond exactly in this JSON format:
 [
-{
-title: "Morning",
-content: [
-"ABC",
-"XYZ",
-],
-places: ["ABC"]
-},
-{
-title: "Afternoon",
-content: [
-"ABC",
-"XYZ",
-"QWE",
-],
-places: ["ABC"]
-},
+  {
+    title: "Section title",
+    content: [
+      "One thing to do in the section",
+      "Another thing to do in the section",
+      "Some information about the places visited during this section",
+      "Another thing to do in the section",
+      
+    ],
+    places: [
+      "Section place highlight 1",
+      "Section place highlight 2",
+    ]
+  },
+  {
+    title: "Section title",
+    content: [
+      "One thing to do in the section",
+      "Another thing to do in the section",
+      "Some information about the places visited during this section",
+      "Another thing to do in the section",
+      
+    ],
+    places: [
+      "Section place highlight 1",
+      "Section place highlight 2",
+    ]
+  },
+  {
+    title: "Section title",
+    content: [
+      "One thing to do in the section",
+      "Another thing to do in the section",
+      "Some information about the places visited during this section",
+      "Another thing to do in the section",
+      
+    ],
+    places: [
+      "Section place highlight 1",
+      "Section place highlight 2",
+    ]
+  },
 ]
 `;
 
@@ -45,24 +75,10 @@ function isValidJsonObject(json: any): boolean {
       typeof item !== "object" ||
       !item.hasOwnProperty("title") ||
       !item.hasOwnProperty("content") ||
-      !item.hasOwnProperty("places") ||
       typeof item.title !== "string" ||
-      !Array.isArray(item.content) ||
-      !Array.isArray(item.places)
+      !Array.isArray(item.content)
     ) {
       return false;
-    }
-
-    for (const contentItem of item.content) {
-      if (typeof contentItem !== "string") {
-        return false;
-      }
-    }
-
-    for (const place of item.places) {
-      if (typeof place !== "string") {
-        return false;
-      }
     }
   }
 
@@ -89,26 +105,26 @@ export default async function getDayByDayPlanUsingOutline({
           content: assistantSystemInput,
         },
         {
-          role: "user",
-          content: `
-          Build plan for day ${day}
-          
+          role: "system",
+          content: `          
           General information about the trip: 
           ${properties.join("; ")}
-
-          Outline:
+          Outline: 
           ${planOutline}
           `,
+        },
+        {
+          role: "user",
+          content: `Build plan for day ${day} of my trip`,
         },
       ];
       promptMessagesByDay.push(promptMessages);
       llmRequests.push(
         openai.chat.completions.create({
-          model: "gpt-3.5-turbo-1106", // Model maximum tokens: 4097
+          model: "gpt-3.5-turbo-1106", // Model maximum tokens: 4095
           messages: promptMessages,
-          temperature: 0, // randomness
-          max_tokens: 600,
-          response_format: { type: "json_object" },
+          temperature: 1, // randomness
+          max_tokens: 4095,
         })
       );
     }
@@ -127,6 +143,7 @@ export default async function getDayByDayPlanUsingOutline({
           return [];
         }
       } catch (error) {
+        console.error(error);
         return [];
       }
     });
